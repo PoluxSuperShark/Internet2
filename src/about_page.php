32fa2e6c33327535348f63dcb6002b6a5d2f746b
PoluxSuperShark <126342301+PoluxSuperShark@users.noreply.github.com>
2026-04-12 13:44:24 +0200

    Delete src directory

--- src/about_page.php
+++
@@ -1,7 +0,0 @@
-<h1>A propos d'Internet 2</h1>
-
-<p><b>Internet 2</b> est un moteur de recherche qui liste les sites crées par les joueurs pour le serveur Minecraft.
- C'est un site gratuit, accessible pour toutes et tous sans soucis. Chaque site est pensé pour être lié au serveur 
-Minecraft.</p>
-
-<p></p>
